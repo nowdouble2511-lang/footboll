c9d2e8420c61b9a3425fcc86ea6fae5254105c2a
nika <nowdouble2511@gmail.com>
2026-03-20 14:47:20 +0300

    1. Изменён MainActivity создали переменные и начали описывать метод getData который проверяет корректный ввод значений. 2. Изменённ gradient.xml. Убрала комментарий. 3. Изменён activity_main.xml. Добавили 3 textView ("Информация о показатели калорий", "Ваш BMR", "Ежедневно тратитсякалорий") и 1 editText ("вывод bmr"-output_bmr)

--- app/src/main/java/com/example/bmr/MainActivity.kt
+++ app/src/main/java/com/example/bmr/MainActivity.kt
@@ -1,20 +1,31 @@
 package com.example.bmr
 
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    lateinit var your_height: EditText
+    lateinit var your_weight: EditText
+    lateinit var your_age: EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        your_height = findViewById(R.id.height)
+        your_weight = findViewById(R.id.weight)
+        your_age = findViewById(R.id.age)
+    }
+
+    fun getData (view: View) {
+        val person_height = your_height.text.toString()
+
     }
 }
